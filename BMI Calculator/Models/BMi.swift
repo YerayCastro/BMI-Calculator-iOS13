ee18a5c88d53fbcd061dc572023aa3b03d85b0e4
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-01-19 11:07:40 +0100

    Commit

---
+++ BMI Calculator/Models/BMi.swift
@@ -0,0 +1,15 @@
+//
+//  BMi.swift
+//  BMI Calculator
+//
+//  Created by Yery Castro on 8/1/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let  value: Float
+    let advice: String
+    let color: UIColor
+}
